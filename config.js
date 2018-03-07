c2a28ee6dfcbcefef352c612013176de4e4ad10e
wbbhacker <2404016117@qq.com>
2018-03-07 10:57:57 +0800

    a

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 const config = {};
-
-<<<<<<< HEAD
 // config.basePath = '/2018/go/0209/wenshi/';
 
 // config.basePath = '/web/sale_go/20180226_heibaoB/';
@@ -10,11 +8,6 @@ const config = {};
 // config.basePath = '/web/sale_go/20180306_demo/';
 
 config.basePath = '/web/sale_go/20180314_Panasonic/';
-=======
->>>>>>> c25c117a85f06ef2fbe4dca8e2fec649efdcede5
-
-config.basePath = '../../svn/go/0209/wenshi/'
-
 
 config.port = 3000;
 
